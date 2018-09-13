df78a7daf880130feacac95d5647980249d5e741
IgnacioPinedo <ignacio.b.pinedo@hotmail.com>
2018-09-13 08:28:25 -0300

    Initial Commit

---
+++ DrinkOrDareApi/Code/Entities/Login/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+public class LoginController : ApiController
+{
+    [HttpGet]
+    [Route("Login")]
+    public IHttpActionResult Login()
+    {
+        return Ok(new
+        {
+            Texto = "Fabio é uma bicha louca !"
+        });
+    }
+
+    [HttpPost]
+    [Route("Login")]
+    public IHttpActionResult Login(JObject json)
+    {
+        var email = json["email"];
+        var nome = json["nome"];
+        var senha = json["senha"];
+
+        return Ok(new
+        {
+            Email = email,
+            Nome = nome,
+            Senha = senha
+        });
+    }
+}
